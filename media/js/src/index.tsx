ee1d8dcf3e42f1b1b1843ba02128312eaaeec297
ndittren <36773036+ndittren@users.noreply.github.com>
2021-11-03 13:53:55 -0400

    Clean up. Login not being used right now

--- media/js/src/index.tsx
+++ media/js/src/index.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../../css/main.css';
 
 const App: React.FC = () => {
-    return (<h1 className='text-danger'>Hello World!</h1>);
+    return (<h1 className='text-success'>Hello World!</h1>);
 };
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
