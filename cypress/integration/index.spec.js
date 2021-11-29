db7a77022461ea54a533e9411b2cece35d143f9d
ndittren <36773036+ndittren@users.noreply.github.com>
2021-11-29 00:14:22 -0500

    Add a quick test to see if it catches error

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,3 +2,9 @@ it('Checks that the site loads', function() {
     let baseUrl = 'http://localhost:8000';
     cy.visit(baseUrl);
 });
+
+it('Checks for start button', function() {
+    let baseUrl = 'http://localhost:8000';
+    cy.visit(baseUrl);
+    cy.get('.btn').contains('Start From Home Page')
+});
