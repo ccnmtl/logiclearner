2d772b198fddeaf7e8d517d97f60837b237aeed3
ndittren <36773036+ndittren@users.noreply.github.com>
2021-12-06 01:35:26 -0500

    mend

--- logiclearner/main/views.py
+++ logiclearner/main/views.py
@@ -1,15 +1,9 @@
 from django.views.generic.base import TemplateView
-from rest_framework import status
-from rest_framework.response import Response
-from django.http import HttpResponseBadRequest
-from django.shortcuts import get_object_or_404, redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.response import Response
 from rest_framework.views import APIView
 # import requests
-# import json
-from django.http import HttpResponseRedirect
+
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
@@ -19,6 +13,11 @@ class SolutionApiView(APIView):
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(SolutionApiView, self).dispatch(*args, **kwargs)
+
+    # def get(self, request):
+    #     requests.request(
+    #         "GET"
+    #     )
 
 
 class HintApiView(APIView):
