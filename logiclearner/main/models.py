1316d1b978b21044d9fda819f297775d5114ce79
ndittren <36773036+ndittren@users.noreply.github.com>
2021-12-17 15:20:12 -0500

    Models created

--- logiclearner/main/models.py
+++ logiclearner/main/models.py
@@ -1,3 +1,27 @@
-# from django.db import models
+from django.db import models
 
-# define your models here
+DIFFICULTY = [
+    (0, 'Beginner'),
+    (1, 'Learner'),
+    (2, 'Apprentice')
+]
+
+class Statement(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    difficulty = models.PositiveSmallIntegerField(
+        choices=DIFFICULTY
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Solution(models.Model):
+    statement = models.ForeignKey(Statement, on_delete=models.CASCADE)
+    ordinal = models.IntegerField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['ordinal']
