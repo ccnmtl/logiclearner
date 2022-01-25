28f4c32dd3d62d8d0edbab84848eab8a89e8a448
ndittren <36773036+ndittren@users.noreply.github.com>
2022-01-25 09:42:29 -0500

    Homepage will be changing. Remove test for now

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,9 +2,3 @@ it('Checks that the site loads', function() {
     let baseUrl = 'http://localhost:8000';
     cy.visit(baseUrl);
 });
-
-it('Checks for start button', function() {
-    let baseUrl = 'http://localhost:8000';
-    cy.visit(baseUrl);
-    cy.get('.btn').contains('Start From Home Page')
-});
