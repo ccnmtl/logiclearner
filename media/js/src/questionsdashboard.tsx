60815cd4eeeca82b069f6aef688ad9d150d7d63b
ndittren <36773036+ndittren@users.noreply.github.com>
2022-02-16 17:09:49 -0500

    Completion count to question dashboard view

--- media/js/src/questionsdashboard.tsx
+++ media/js/src/questionsdashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { getStatements, Statement } from './utils';
+import { getStatements, Statement, ExerciseData,
+    completionCount } from './utils';
 import { Question } from './question';
 
 interface QuestionsDashboardProps {
@@ -11,15 +12,37 @@ export const QuestionsDashboard: React.FC<QuestionsDashboardProps> = (
     {difficulty, level}: QuestionsDashboardProps) => {
 
     const [statements, setStatements] = useState<Statement[]>([]);
+    const [questionsList, setquestionsList] = useState([]);
+    const [levelCount, setLevelCount] = useState<number>(0);
 
     async function fetchStatements() {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const json: Array<Statement> = await getStatements(difficulty);
         setStatements(json);
     }
+    const getQuestionList = () => {
+        const keys = Object.keys(localStorage);
+        const arrQs = [];
+        for (const key of keys) {
+            if(key.includes('question-')) {
+                const item = JSON.parse(
+                    window.localStorage.getItem(key)) as ExerciseData[];
+                arrQs.push(item);
+            }
+        }
+        setquestionsList(arrQs);
+    };
+    async function fetchCounts() {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const novArr: Array<Statement>  = await getStatements(difficulty);
+        setLevelCount(novArr.length);
+    }
+    const completedCount = completionCount(level, questionsList);
 
     useEffect(() => {
         void fetchStatements();
+        void getQuestionList();
+        void fetchCounts();
     }, []);
 
     return (
@@ -27,10 +50,21 @@ export const QuestionsDashboard: React.FC<QuestionsDashboardProps> = (
             <div className="d-flex flex-column min-vh-100 justify-content-center
                             align-items-center"
             data-testid={'QuestionDashboard'}>
-                <p>Level {difficulty + 1}</p>
-                <p className='h2'>{level}</p>
-                <p>One sentence description here</p>
-
+                <div className='container'>
+                    <div className='row justify-content-center'>
+                        <div className='col-4'>
+                            <p>Level {difficulty + 1}
+                                <span className='float-end'>
+                                    {completedCount}/{levelCount}
+                                </span>
+                            </p>
+                            <p className='h2'>{level}</p>
+                            <p>One sentence description here</p>
+                        </div>
+                        <div className='col-4'>
+                        </div>
+                    </div>
+                </div>
                 {statements.map((statement, idx) => {
                     return (<Question
                         statement={statement}
