19b6d691463e48b890f6bf3fd6036f46194b631f
Zarina Mustapha <zarina@columbia.edu>
2022-02-17 18:28:29 -0500

    Home-Levels page view

---
+++ media/js/src/homeBanner.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { HomeBanner } from './homebanner';
+
+describe('Initial test', () => {
+    const renderComponent = () => render(<HomeBanner />);
+    it('Should render the HomeBanner component', () => {
+        const { getByTestId } = renderComponent();
+        const component = getByTestId('HomeBanner');
+        expect(component).toBeVisible();
+    });
+});
---
+++ media/js/src/homebanner.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const HomeBanner: React.FC = () => {
+
+    return (<>
+        <header className="main-banner banner-hero">
+            <div className="container
+                d-flex justify-content-center
+                banner-hero__container">
+                <figure className="main-banner__avatar
+                    banner-hero__avatar align-self-center"
+                aria-hidden="true">
+                    <img src="media/img/temp-icon-sonobe.svg" />
+                </figure>
+                <h1 className="align-self-center">
+                    <span className="main-banner__title
+                        banner-hero__title">Logic Learner</span>
+                    <span className="main-banner__text
+                        banner-hero__text">Logic is the beginning of wisdom,
+                        not the end of it</span>
+                </h1>
+            </div>
+        </header>
+    </>);
+};
