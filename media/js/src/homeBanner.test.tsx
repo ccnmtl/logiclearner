d2584b0f19a1828eeaca1763186b003dfc621703
ndittren <36773036+ndittren@users.noreply.github.com>
2022-02-23 11:00:04 -0500

    Removed camelcase

--- media/js/src/homeBanner.test.tsx
+++ media/js/src/homeBanner.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { HomeBanner } from './homeBanner';
+import { HomeBanner } from './homebanner';
 
 describe('Initial test', () => {
     const renderComponent = () => render(<HomeBanner />);
