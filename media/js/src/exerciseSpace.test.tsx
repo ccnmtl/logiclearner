d6c4111b7715618b5223d26879c10b7646c2ce87
ndittren <36773036+ndittren@users.noreply.github.com>
2022-03-12 17:14:17 -0500

    Fixes console error

--- media/js/src/exerciseSpace.test.tsx
+++ media/js/src/exerciseSpace.test.tsx
@@ -4,7 +4,7 @@ import { ExerciseSpace } from './exerciseSpace';
 
 describe('Initial test', () => {
     const renderComponent = () => render(<ExerciseSpace />);
-
+    window.scrollTo = jest.fn();
     it('Should render the Question component', () => {
         const { getByTestId } = renderComponent();
         const component = getByTestId('exerciseSpace');
