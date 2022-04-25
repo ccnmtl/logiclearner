18c78cdae8b255803f3045fc20f356468496f8f3
ndittren <36773036+ndittren@users.noreply.github.com>
2022-04-25 09:41:52 -0400

    Serializer and basic view tests

--- logiclearner/main/tests/test_views.py
+++ logiclearner/main/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from logiclearner.main.tests.factories import SolutionFactory, StatementFactory
 
 
 class BasicTest(TestCase):
@@ -10,3 +11,29 @@ class BasicTest(TestCase):
         response = self.client.get("/smoketest/")
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'PASS')
+
+
+class StatementListAPIViewTest(TestCase):
+    def setUp(self):
+        self.statement = StatementFactory()
+        self.statement1 = StatementFactory()
+
+    def test_get(self):
+        response = self.client.get('/api/statements/0/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['question'], 'F->T')
+
+
+class SolutionListAPIView(TestCase):
+    def setUp(self):
+        self.statement = StatementFactory()
+        self.solution = SolutionFactory(statement=self.statement)
+        self.solution1 = SolutionFactory(statement=self.statement)
+
+    def test_get(self):
+        response = self.client.get('/api/solution/{}/'.format(
+                                                            self.statement.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['ordinal'], 1)
