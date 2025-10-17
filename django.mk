dadb2de44a4390aef3958f31e52cc386beb415d5
ndittren <36773036+ndittren@users.noreply.github.com>
2025-10-17 09:03:45 -0400

    Remove Coverage

--- django.mk
+++ django.mk
@@ -1,7 +1,7 @@
-# VERSION=1.9.0
+# VERSION=1.10.0
 
 # CHANGES:
-# 1.9.0              - Use coverage tool directly to generate coverage
+# 1.10.0 - 2025-10-16 - Removed coverage, updated dependencies.
 #                      reports.
 #                    - wheel and pip updates
 #                    - Use pre-compiled binary wheel for cryptography
@@ -24,7 +24,7 @@ REQUIREMENTS ?= requirements.txt
 SYS_PYTHON ?= python3
 PY_SENTINAL ?= $(VE)/sentinal
 WHEEL_VERSION ?= 0.44.0
-PIP_VERSION ?= 24.2
+PIP_VERSION ?= 25.2
 MAX_COMPLEXITY ?= 10
 INTERFACE ?= localhost
 RUNSERVER_PORT ?= 8000
@@ -32,7 +32,6 @@ PY_DIRS ?= $(APP)
 BANDIT ?= $(VE)/bin/bandit
 FLAKE8 ?= $(VE)/bin/flake8
 PIP ?= $(VE)/bin/pip
-COVERAGE ?= $(VE)/bin/coverage
 
 jenkins: check flake8 test bandit
 
@@ -46,8 +45,7 @@ $(PY_SENTINAL): $(REQUIREMENTS)
 	touch $@
 
 test: $(PY_SENTINAL)
-	$(COVERAGE) run --source='.' --omit=$(VE)/* $(MANAGE) test $(PY_DIRS)
-	$(COVERAGE) xml -o reports/coverage.xml
+	$(MANAGE) test $(PY_DIRS)
 
 parallel-tests: $(PY_SENTINAL)
 	$(MANAGE) test --parallel
@@ -75,7 +73,6 @@ clean:
 	rm -rf media/CACHE
 	rm -rf reports
 	rm -f celerybeat-schedule
-	rm -f .coverage
 	rm -rf node_modules
 	find . -name '*.pyc' -exec rm {} \;
 
